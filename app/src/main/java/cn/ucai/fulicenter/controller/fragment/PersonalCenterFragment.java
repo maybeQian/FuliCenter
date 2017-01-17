3083ce5a0ef7e5ccf420052b8724250fcab04433
xiongqian <736182683@qq.com>
2017-01-17 14:55:11 +0800

    个人中心数据显示

--- app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
@@ -6,14 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuliCenterApplication;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
+import cn.ucai.fulicenter.view.MFGT;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersonalCenterFragment extends Fragment {
 
+
+    @BindView(R.id.iv_user_avatar)
+    ImageView mivUserAvatar;
+    @BindView(R.id.tv_user_name)
+    TextView mtvUserName;
 
     public PersonalCenterFragment() {
     }
@@ -22,7 +35,24 @@ public class PersonalCenterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_personal_center, container, false);
+        View view = inflater.inflate(R.layout.fragment_personal_center, container, false);
+        ButterKnife.bind(this, view);
+        initData();
+        return view;
+    }
+
+    private void initData() {
+        User user = FuliCenterApplication.getUser();
+        if (user != null) {
+            loadUserInfo(user);
+        } else {
+            MFGT.gotoLogin(getActivity());
+        }
+    }
+
+    private void loadUserInfo(User user) {
+        ImageLoader.downloadImg(getContext(),mivUserAvatar,user.getAvatarPath());
+        mtvUserName.setText(user.getMuserNick());
     }
 
 }
