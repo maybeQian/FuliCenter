4817cac8dd172e1bcecd75992964fcd62e690a81
xiongqian <736182683@qq.com>
2017-01-19 14:26:00 +0800

    商品收藏页面的数据显示和商品的删除收藏操作

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/CollectsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/CollectsActivity.java
@@ -3,8 +3,10 @@ package cn.ucai.fulicenter.controller.activity;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,13 +17,19 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.FuliCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.controller.adapter.CollectsAdapter;
+import cn.ucai.fulicenter.controller.adapter.NewGoodsAdapter;
 import cn.ucai.fulicenter.model.bean.CollectBean;
+import cn.ucai.fulicenter.model.bean.NewGoodsBean;
 import cn.ucai.fulicenter.model.bean.User;
 import cn.ucai.fulicenter.model.net.IModelUser;
 import cn.ucai.fulicenter.model.net.ModelUser;
 import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
 import cn.ucai.fulicenter.model.utils.ConvertUtils;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
 import cn.ucai.fulicenter.view.MFGT;
+import cn.ucai.fulicenter.view.SpaceItemDecoration;
 
 public class CollectsActivity extends AppCompatActivity {
 
@@ -32,9 +40,13 @@ public class CollectsActivity extends AppCompatActivity {
     @BindView(R.id.srl)
     SwipeRefreshLayout mSrl;
 
+    CollectsAdapter mAdapter;
+    ArrayList<CollectBean> mList;
+    GridLayoutManager mLayoutManager;
+
     User user;
     IModelUser mModel;
-    int pageId=1;
+    int mPageId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,26 +56,112 @@ public class CollectsActivity extends AppCompatActivity {
         if (user == null) {
             finish();
         } else {
+            initView();
             initData();
+            setListener();
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initData();
+    }
+
+    private void setListener() {
+        setPullDownListener();
+        setPullUpListener();
+    }
+
+    private void setPullUpListener() {
+        mrvGoods.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int lastPosition=mLayoutManager.findLastVisibleItemPosition();
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && mAdapter.isMore() && lastPosition == mAdapter.getItemCount() - 1) {
+                    mPageId++;
+                    downloadData(I.ACTION_PULL_UP,mPageId);
+                }
+            }
+        });
+    }
+
+    private void setPullDownListener() {
+        mSrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSrl.setRefreshing(true);
+                mtvRefreshHint.setVisibility(View.VISIBLE);
+                mPageId=1;
+                downloadData(I.ACTION_PULL_DOWN,mPageId);
+            }
+        });
+    }
+    private void initView() {
+        mSrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_yellow),
+                getResources().getColor(R.color.google_red) );
+        mList=new ArrayList<>();
+        mAdapter = new CollectsAdapter(this, mList);
+        mrvGoods.setAdapter(mAdapter);
+        mLayoutManager = new GridLayoutManager(this, I.COLUM_NUM);
+        mrvGoods.setLayoutManager(mLayoutManager);
+        mrvGoods.setHasFixedSize(true);
+        mrvGoods.addItemDecoration(new SpaceItemDecoration(30));
+
+        mLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position == mAdapter.getItemCount() - 1) {
+                    return I.COLUM_NUM;
+                }
+                return 1;
+            }
+        });
+    }
+
     private void initData() {
+        mPageId=1;
+        downloadData(I.ACTION_DOWNLOAD, mPageId);
+    }
+
+    private void downloadData(final int action, int mPageId) {
         mModel=new ModelUser();
-        mModel.getCollects(this, user.getMuserName(), pageId, I.PAGE_SIZE_DEFAULT, new OnCompleteListener<CollectBean[]>() {
+        mModel.getCollects(this, user.getMuserName(), mPageId, I.PAGE_SIZE_DEFAULT, new OnCompleteListener<CollectBean[]>() {
             @Override
             public void onSuccess(CollectBean[] result) {
-                if (result != null) {
-                    ArrayList<CollectBean> list = ConvertUtils.array2List(result);
-                    Log.e("main", "collects.list=" + list.size());
-                } else {
+                mAdapter.setMore(result!=null && result.length>0);
+                if (!mAdapter.isMore()) {
+                    if (action == I.ACTION_PULL_UP) {
+                        mAdapter.setFooter("没有更多数据");
+                    }
+                    return;
+                }
+                mAdapter.setFooter("上拉加载更多数据");
+                ArrayList<CollectBean> list = ConvertUtils.array2List(result);
+                switch (action) {
+                    case I.ACTION_DOWNLOAD:
+                        mAdapter.initData(list);
+                        break;
+                    case I.ACTION_PULL_DOWN:
+                        ImageLoader.release();
+                        mSrl.setRefreshing(false);
+                        mtvRefreshHint.setVisibility(View.GONE);
+                        mAdapter.initData(list);
+                        break;
+                    case I.ACTION_PULL_UP:
+                        mAdapter.addData(list);
+                        break;
 
                 }
             }
 
             @Override
             public void onError(String error) {
-
+                CommonUtils.showLongToast(error);
             }
         });
     }
---
+++ app/src/main/java/cn/ucai/fulicenter/controller/adapter/CollectsAdapter.java
@@ -0,0 +1,182 @@
+package cn.ucai.fulicenter.controller.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuliCenterApplication;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.CollectBean;
+import cn.ucai.fulicenter.model.bean.MessageBean;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IModelGoodsDetail;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelGoodsDetail;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
+import cn.ucai.fulicenter.view.MFGT;
+
+/**
+ * Created by Administrator on 2017/1/19 0019.
+ */
+
+public class CollectsAdapter extends RecyclerView.Adapter {
+    public static final int TYPE_GOODS = 0;
+    public static final int TYPE_FOOTER = 1;
+    Context mContext;
+    ArrayList<CollectBean> mList;
+    String footer;
+    boolean isMore;
+
+    IModelGoodsDetail mModel;
+    User user;
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+        notifyDataSetChanged();
+    }
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
+
+    public CollectsAdapter(Context mContext, ArrayList<CollectBean> list) {
+        this.mContext = mContext;
+        this.mList = list;
+        mModel=new ModelGoodsDetail();
+        user= FuliCenterApplication.getUser();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View layout;
+        switch (viewType) {
+            case TYPE_GOODS:
+                layout = inflater.inflate(R.layout.item_collects, null);
+                return new CollectsViewHolder(layout);
+            case TYPE_FOOTER:
+                layout = inflater.inflate(R.layout.item_footer, null);
+                return new FooterViewHolder(layout);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder parentHolder, int position) {
+        if (getItemViewType(position) == TYPE_FOOTER) {
+            FooterViewHolder holder = (FooterViewHolder) parentHolder;
+            holder.tvFooter.setText(getFooter());
+            return;
+        }
+        final CollectsViewHolder holder = (CollectsViewHolder) parentHolder;
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return TYPE_FOOTER;
+        }
+        return TYPE_GOODS;
+    }
+
+    public void initData(ArrayList<CollectBean> list) {
+        if (list != null) {
+            this.mList.clear();
+        }
+        addData(list);
+    }
+
+    public void addData(ArrayList<CollectBean> list) {
+        this.mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+
+
+    static class FooterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tvFooter)
+        TextView tvFooter;
+
+        FooterViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class CollectsViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivGoodsThumb)
+        ImageView ivGoodsThumb;
+        @BindView(R.id.ivCollectsDelete)
+        ImageView ivCollectsDelete;
+        @BindView(R.id.tvGoodsName)
+        TextView tvGoodsName;
+        @BindView(R.id.layout_goods)
+        LinearLayout layoutGoods;
+
+        int itemPosition;
+
+        CollectsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        public void bind(int position) {
+            final CollectBean collectBean = mList.get(position);
+            tvGoodsName.setText(collectBean.getGoodsName());
+            ImageLoader.downloadImg(mContext, ivGoodsThumb, collectBean.getGoodsThumb());
+            itemPosition=position;
+        }
+
+        @OnClick({R.id.ivCollectsDelete, R.id.layout_goods})
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.ivCollectsDelete:
+                    mModel.setCollect(mContext, mList.get(itemPosition).getGoodsId(),
+                            user.getMuserName(), I.ACTION_DELETE_COLLECT, new OnCompleteListener<MessageBean>() {
+                                @Override
+                                public void onSuccess(MessageBean result) {
+                                    if (result != null && result.isSuccess()) {
+                                        mList.remove(itemPosition);
+                                        notifyDataSetChanged();
+                                    }
+                                }
+
+                                @Override
+                                public void onError(String error) {
+
+                                }
+                            });
+                    break;
+                case R.id.layout_goods:
+                    MFGT.gotoGoodsDetail(mContext,mList.get(itemPosition).getGoodsId());
+                    break;
+            }
+        }
+    }
+}
